79abbf3ef98a277c0d941c3edda3df5ef2d8185f
Artyom Ponedelchuk <ponart95@gmail.com>
2023-09-28 12:59:15 +0300

    add_logic_for_director

--- customers/models.py
+++ customers/models.py
@@ -1,5 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Customer(models.Model):
     email = models.CharField(max_length=255,blank=False, null=False)
+
+class Director(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_director = models.BooleanField(default=False)
+   
+    def __str__(self):
+        return self.user.username
--- robots/decorators.py
+++ robots/decorators.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseForbidden
 
 
 def admin_required(view_func):
@@ -9,4 +9,15 @@ def admin_required(view_func):
             return view_func(request, *args, **kwargs)
         else:
             return JsonResponse({'error': 'Access denied. This action requires admin privileges.'}, status=403)
+    return _wrapped_view
+
+def director_required(view_func):
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        # Проверяем, является ли текущий пользователь директором
+        if request.user.is_authenticated and request.user.director.is_director:
+            return view_func(request, *args, **kwargs)
+        else:
+            return HttpResponseForbidden("Permission Denied: You are not a director.")
+
     return _wrapped_view
